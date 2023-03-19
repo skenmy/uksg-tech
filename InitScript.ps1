e9336e4ecea9aace01be77899cdbcea729953bcc
Paul Williams <paul@skenmy.com>
2023-03-19 20:29:07 +0000

    Update InitScript.ps1

--- InitScript.ps1
+++ InitScript.ps1
@@ -1,10 +1,14 @@
-# UKSG Tech Initialization Script
+# UKSG Tech Installer / Upgrader Script
 # Written by Paul Williams <paul@skenmy.com>
 
-# This script automates the download of the UKSG Tech
-# Installer & Updater Script
+# This script automates the installation of a number of
+# software packages that the Tech PC needs. It is designed to
+# keep things up to date in a repeatable and predictable way.
+# It will also guide you through any manual steps required
+# to get the PC ready for an event.
 
 ## Function Definitions
+
 function Set-Environment {
     Write-Host "Refreshing Environment"
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
@@ -59,6 +63,7 @@ function Invoke-Command-Surround-Output-Ignore-Exit {
 }
 
 ## Execution
+
 if (!
     (New-Object Security.Principal.WindowsPrincipal(
         [Security.Principal.WindowsIdentity]::GetCurrent()
@@ -72,23 +77,36 @@ if (!
     Exit-After-Keypress
 }
 
-Write-Header "UKSG Tech - PC Initialization Script"
-Write-Host -ForegroundColor Black -BackgroundColor Red "Do not execute this script unless this is a brand new, unconfigured PC!"
-Enter-After-Keypress
-Write-Host "Please select the directory to use!"
-Add-Type -AssemblyName System.Windows.Forms
-$browser = New-Object System.Windows.Forms.FolderBrowserDialog
-$null = $browser.ShowDialog()
-$path = $browser.SelectedPath
-Write-Host "Using "$path
+Write-Header "UKSG Tech Installer / Upgrader"
+Write-Host "Running in" $PSScriptRoot
+Write-Host "NodeCG will be set up & configured in" $PSScriptRoot"\nodecg"
+Write-Host -ForegroundColor Yellow "This script may need to be run multiple times - it will tell you if this is the case."
+Write-Host -ForegroundColor Red "Do not execute this script unless you have been told to run it, or you know what it does!"
+Write-Host -ForegroundColor Black -BackgroundColor Red "Do not execute this script if you are currently running a marathon - bad things will happen!"
 Enter-After-Keypress
 
 # Set System Settings
 Write-Header "System Settings"
+Write-Host "Setting Working Directory..."
+Set-Location -Path $PSScriptRoot
+Write-Host -ForegroundColor Green "Success"
+
+Write-Host "Set Directory Safety"
+Invoke-Command-Surround-Output git config --global --add safe.directory $PSScriptRoot
+Write-Host -ForegroundColor Green "Success"
 
 Write-Host "Setting ExecutionPolicy to Bypass..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+Write-Host -ForegroundColor Green "Success"
+
+Write-Host "Setting System Timezone..."
+Set-TimeZone -Id "GMT Standard Time"
+Write-Host -ForegroundColor Green "Success"
+
+Write-Host "Setting System Locale & Input Settings..."
+Set-WinSystemLocale en-GB
+Set-WinDefaultInputMethodOverride -InputTip "0809:00000809" # en-GB Keyboard Layout
 Write-Host -ForegroundColor Green "Success"
 
 # Install / Upgrade Chocolatey
@@ -114,18 +132,103 @@ Write-Host "Installing / Updating Git..."
 Invoke-Command-Surround-Output choco upgrade git -y
 Set-Environment
 
-Write-Host "Setting Working Directory..."
-Set-Location -Path $path
-Write-Host -ForegroundColor Green "Success"
+Write-Host "Checking for script updates..."
+Invoke-Command-Surround-Output git fetch
 
-Write-Host "Downloading Scripts & Configs..."
-Invoke-Command-Surround-Output git clone https://github.com/skenmy/uksg-tech.git .
-Write-Host -ForegroundColor Green "Success"
+$gitBehind = cmd.exe /c 'git status | find /i "Your branch is behind"'
+if ($gitBehind) {
+    Write-Host -ForegroundColor Red "Updates found... downloading..."
+    Invoke-Command-Surround-Output git pull
+    Write-Host -ForegroundColor Yellow "You must restart this script to run the updated version!"
+    Exit-After-Keypress
+}
 
-Write-Host "Set Directory Safety"
-Invoke-Command-Surround-Output git config --global --add safe.directory $path
-Write-Host -ForegroundColor Green "Success"
+# Install / Upgrade Packages
+Write-Header "Packages & Software"
 
+# Install / Upgrade VSCode
+Write-Host "Installing / Updating VSCode..."
+Invoke-Command-Surround-Output choco upgrade vscode -y
+
+# Install / Upgrade AutoHotkey
+Write-Host "Installing / Updating AutoHotkey..."
+Invoke-Command-Surround-Output choco upgrade autohotkey -y
+
+# Install / Upgrade OBS
+Write-Host "Installing / Updating OBS..."
+Invoke-Command-Surround-Output choco upgrade obs-studio -y
+
+# Install / Upgrade foobar2000
+Write-Host "Installing / Updating foobar2000..."
+Invoke-Command-Surround-Output choco upgrade foobar2000 -y
+
+# Install / Upgrade X AIR Edit
+Write-Host "Installing / Updating X AIR Edit..."
+Invoke-Command-Surround-Output choco upgrade x-air-edit -y
+
+# Install / Upgrade NodeJS LTS
+Write-Host "Installing / Updating NodeJS (LTS)..."
+Invoke-Command-Surround-Output choco upgrade nodejs-lts -y
+
+Set-Environment
+if ( -not (Get-Command npm -errorAction SilentlyContinue)) {
+    Write-Host -ForegroundColor Red "NPM not found in PATH - check NodeJS installation"
+    Write-Host "This is a problem, but might be solved by just trying again."
+    Write-Host -ForegroundColor Yellow "If you see this error repeatedly - talk to a (very) Senior Tech!"
+    Exit-After-Keypress
+}
+
+Write-Host "Installing / Updating NPM..."
+Invoke-Command-Surround-Output npm install -g npm
+
+Write-Host "Installing / Updating Bower..."
+Invoke-Command-Surround-Output npm install -g bower
+
+# Install / Upgrade NodeCG
+Write-Header "NodeCG Install & Configuration"
+Write-Host "Installing / Updating NodeCG CLI..."
+Invoke-Command-Surround-Output npm install -g nodecg-cli
+
+if (-not (Test-Path -Path $PSScriptRoot\nodecg)) {
+    Write-Host "Creating NodeCG Directory..."
+    New-Item -Path $PSScriptRoot -Name "nodecg" -ItemType "directory"
+    Write-Host "First time NodeCG Setup..."
+    Set-Location -Path $PSScriptRoot\nodecg
+    Invoke-Command-Surround-Output nodecg setup
+}
+
+Write-Host "Installing / Updating nodecg-speedcontrol..."
+Set-Location -Path $PSScriptRoot\nodecg\bundles
+Invoke-Command-Surround-Output git clone https://github.com/speedcontrol/nodecg-speedcontrol.git .
+Set-Location -Path $PSScriptRoot\nodecg\bundles\nodecg-speedcontrol
+Invoke-Command-Surround-Output git checkout build
+Invoke-Command-Surround-Output npm i -production
+
+Write-Host "Installing / Updating esa-layouts..."
+Set-Location -Path $PSScriptRoot\nodecg\bundles
+Invoke-Command-Surround-Output git clone https://github.com/esamarathon/esa-layouts.git .
+Set-Location -Path $PSScriptRoot\nodecg\bundles\esa-layouts
+Invoke-Command-Surround-Output git checkout build
+Invoke-Command-Surround-Output npm i -production
+
+Write-Host "Installing / Updating esa-featuredchannels..."
+Set-Location -Path $PSScriptRoot\nodecg\bundles
+Invoke-Command-Surround-Output git clone https://github.com/esamarathon/esa-featuredchannels.git .
+Set-Location -Path $PSScriptRoot\nodecg\bundles\esa-featuredchannels
+Invoke-Command-Surround-Output git checkout build
+Invoke-Command-Surround-Output npm i -production
+
+Write-Host "Installing / Updating esa-commercials..."
+Set-Location -Path $PSScriptRoot\nodecg\bundles
+Invoke-Command-Surround-Output git clone https://github.com/esamarathon/esa-commercials.git .
+Set-Location -Path $PSScriptRoot\nodecg\bundles\esa-commercials
+Invoke-Command-Surround-Output git checkout build
+Invoke-Command-Surround-Output npm i -production
+
+# Manual Steps
+Write-Header "Manual Installation Steps"
+
+# Finished
 Write-Header "All Done!"
-Write-Host -ForegroundColor Green "You can now use the scripts in "$path" to setup this computer!"
+Write-Host -ForegroundColor Green "UKSG Installer / Updater has completed!"
 Exit-After-Keypress
